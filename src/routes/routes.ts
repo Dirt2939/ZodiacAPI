507f58793bf6fa81dce2685cccbf2f8ee4dfd009
Dirt2939 <rafaelhunemeier@gmail.com>
2026-08-13 17:45:54 -0300

    feat: add error handling middleware

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import geoData from "../geoData/geoData.routes";
+import { errorHandler } from "../shared/middlewares/ErrorHandler/errorHandler";
 
 const router = Router()
 router.use("/Geo", geoData)
+router.use(errorHandler)
 
 export default router
